eb4432ac2f20a7f2c7491e3974fc69c9f83cde31
marvinbuss <marvin.buss24@gmail.com>
2022-02-16 13:27:36 +0100

    add PostCard component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,8 @@
 import Head from 'next/head'
 import { PostCard, Categories, PostWidget } from '../components'
+import { getPosts } from '../services'
 
-const posts = [
-  { title: 'Cryptoland', excerpt: 'Learn to create a web 3.0 app' },
-  {
-    title: 'Learn Solitidy',
-    excerpt: 'Learn to write Solitidy using Hardhat',
-  },
-]
-
-export default function Home() {
+export default function Home({ posts }) {
   return (
     <div className="container mx-auto mb-8 px-10">
       <Head>
@@ -19,13 +12,23 @@ export default function Home() {
       <div className="grid grid-cols-1 gap-12 lg:grid-cols-12">
         <div className="col-span-1 lg:col-span-8">
           {posts.map((post, index) => (
-            <PostCard post={post} key={post.title} />
+            <PostCard post={post.node} key={post.node.title} />
           ))}
         </div>
         <div className="col-span-1 lg:col-span-4">
-          <div className="relative top-8 lg:sticky"></div>
+          <div className="relative top-8 lg:sticky">
+            <PostWidget />
+            <Categories />
+          </div>
         </div>
       </div>
     </div>
   )
+}
+
+export async function getStaticProps() {
+  const posts = (await getPosts()) || []
+  return {
+    props: { posts },
+  }
 }
